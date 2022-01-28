446a45761cc05bade95b9dbb9999d0a98aabc9b2
irgaly <irgaly@gmail.com>
2022-01-28 21:55:34 +0900

    bump 1.3.1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,7 +10,7 @@ pluginManagement {
         id("com.android.application") version "7.0.1"
         id("com.android.library") version "7.0.1"
         kotlin("android") version "1.6.10"
-        id("io.github.irgaly.remove-unused-resources") version "1.3.0"
+        id("io.github.irgaly.remove-unused-resources") version "1.3.1"
     }
 }
 dependencyResolutionManagement {
