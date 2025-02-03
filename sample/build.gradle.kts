3a7f9db27bc5a254e51b2d996c33a45d307a86b8
irgaly <irgaly@gmail.com>
2025-02-03 23:24:12 +0900

    compileSdk = 35, targetSdk = 35

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -7,11 +7,11 @@ plugins {
 
 android {
     namespace = "org.sample.app"
-    compileSdk = 34
+    compileSdk = 35
     defaultConfig {
         applicationId = "org.sample.app"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0.0"
     }
