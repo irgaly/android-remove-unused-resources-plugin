2317ca7f0d2035f58366313565904f9b27e0dc5d
irgaly <irgaly@gmail.com>
2025-10-12 23:11:36 +0900

    sample: use ComponentActivity

--- sample/src/main/kotlin/org/sample/app/MainActivity.kt
+++ sample/src/main/kotlin/org/sample/app/MainActivity.kt
@@ -1,9 +1,9 @@
 package org.sample.app
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
@@ -14,7 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.core.view.WindowInsetsControllerCompat
 
 @Suppress("UNUSED_VARIABLE", "LocalVariableName")
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
